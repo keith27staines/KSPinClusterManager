b806cee068d0b9690259f723aa9fc48638ed0da7
Keith Staines <keith27staines@icloud.com>
2020-06-22 22:38:10 +0100

    Initial Commit

---
+++ KSPinClusterManager/KSPinClusterManager.h
@@ -0,0 +1,19 @@
+//
+//  KSPinClusterManager.h
+//  KSPinClusterManager
+//
+//  Created by Keith Staines on 22/06/2020.
+//  Copyright © 2020 Keith Staines. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSPinClusterManager.
+FOUNDATION_EXPORT double KSPinClusterManagerVersionNumber;
+
+//! Project version string for KSPinClusterManager.
+FOUNDATION_EXPORT const unsigned char KSPinClusterManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSPinClusterManager/PublicHeader.h>
+
+
